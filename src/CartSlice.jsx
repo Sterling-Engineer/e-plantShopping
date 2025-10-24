591c90d635a3e2fe136576836f7e041fdc7c7a31
Sterling-Engineer <bobsterlingit@gmail.com>
2025-10-23 22:40:12 -0400

    Contents of e-plantShopping application

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,23 +1,24 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
-export const CartSlice = createSlice({
-  name: 'cart',
+const cartSlice = createSlice({
+  name: "cart",
   initialState: {
-    items: [], // Initialize items as an empty array
+    items: [],
   },
   reducers: {
     addItem: (state, action) => {
-    
+      state.items.push(action.payload);
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter(
+        (item) => item.name !== action.payload.name
+      );
     },
-    updateQuantity: (state, action) => {
-
-    
+    clearCart: (state) => {
+      state.items = [];
     },
   },
 });
 
-export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
-
-export default CartSlice.reducer;
+export const { addItem, removeItem, clearCart } = cartSlice.actions;
+export default cartSlice.reducer;
